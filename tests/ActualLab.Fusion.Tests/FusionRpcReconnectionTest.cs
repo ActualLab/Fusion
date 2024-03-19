06bd81614bc627750e774ac0861d68d92b1f4022
Alex Yakunin <alex.yakunin@gmail.com>
2024-03-19 03:04:17 -0700

    chore: fix timeout in FusionRpcReconnectionTest.cs

--- tests/ActualLab.Fusion.Tests/FusionRpcReconnectionTest.cs
+++ tests/ActualLab.Fusion.Tests/FusionRpcReconnectionTest.cs
@@ -150,7 +150,7 @@ public class FusionRpcReconnectionTest(ITestOutputHelper @out) : SimpleFusionTes
             else {
                 Out.WriteLine($"{i}: Syncing...");
                 var c = await Computed.Capture(() => client.GetTime());
-                await c.When(x => x == st).WaitAsync(TimeSpan.FromSeconds(1));
+                await c.When(x => x == st).WaitAsync(TimeSpan.FromSeconds(2));
                 Out.WriteLine($"{i}: Synced");
             }
         }
