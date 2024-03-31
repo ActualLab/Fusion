2a3764b00e1d4854801c1a3ac1ba2b080fbb8aa7
Alex Yakunin <alex.yakunin@gmail.com>
2024-03-31 03:19:58 -0700

    feat: RpcOutboundCommandCallMiddleware - add separate timeouts for backend calls

--- src/ActualLab.CommandR/Rpc/RpcOutboundCommandCallMiddleware.cs
+++ src/ActualLab.CommandR/Rpc/RpcOutboundCommandCallMiddleware.cs
@@ -5,11 +5,15 @@ namespace ActualLab.CommandR.Rpc;
 public class RpcOutboundCommandCallMiddleware : RpcOutboundMiddleware
 {
     public static TimeSpan DefaultConnectTimeout { get; set; } = TimeSpan.FromSeconds(1.5);
-    public static TimeSpan DefaultTimeout { get; set; } = TimeSpan.FromSeconds(10);
+    public static TimeSpan DefaultCallTimeout { get; set; } = TimeSpan.FromSeconds(10);
+    public static TimeSpan DefaultBackendConnectTimeout { get; set; } = TimeSpan.FromMinutes(5);
+    public static TimeSpan DefaultBackendCallTimeout { get; set; } = TimeSpan.FromMinutes(5);
     public int DefaultPriority { get; set; } = 10;
 
     public TimeSpan ConnectTimeout { get; set; } = DefaultConnectTimeout;
-    public TimeSpan Timeout { get; set; } = DefaultTimeout;
+    public TimeSpan CallTimeout { get; set; } = DefaultCallTimeout;
+    public TimeSpan BackendConnectTimeout { get; set; } = DefaultBackendConnectTimeout;
+    public TimeSpan BackendCallTimeout { get; set; } = DefaultBackendCallTimeout;
 
     public RpcOutboundCommandCallMiddleware(IServiceProvider services)
         : base(services)
@@ -21,12 +25,17 @@ public class RpcOutboundCommandCallMiddleware : RpcOutboundMiddleware
         var parameterTypes = methodDef!.ParameterTypes;
         if (parameterTypes.Length is < 1 or > 2)
             return;
-
         if (!typeof(ICommand).IsAssignableFrom(parameterTypes[0]))
             return;
 
         var call = context.Call!;
-        call.ConnectTimeout = ConnectTimeout;
-        call.Timeout = Timeout;
+        if (methodDef.IsBackend) {
+            call.ConnectTimeout = BackendConnectTimeout;
+            call.Timeout = BackendCallTimeout;
+        }
+        else {
+            call.ConnectTimeout = ConnectTimeout;
+            call.Timeout = CallTimeout;
+        }
     }
 }
