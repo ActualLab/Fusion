ec0941239856b900c2adf709d2bba021b69a4288
Frol <4425748+FrolYo@users.noreply.github.com>
2024-04-18 11:35:49 +0300

    chore: retry delay seq test - first value

--- tests/ActualLab.Tests/Time/RetryDelaySeqTest.cs
+++ tests/ActualLab.Tests/Time/RetryDelaySeqTest.cs
@@ -1,4 +1,4 @@
-namespace ActualLab.Tests.Rpc;
+namespace ActualLab.Tests.Time;
 
 public class RetryDelaySeqTest(ITestOutputHelper @out) : TestBase(@out)
 {
@@ -15,5 +15,18 @@ public class RetryDelaySeqTest(ITestOutputHelper @out) : TestBase(@out)
         s[0].TotalSeconds.Should().Be(0);
         s[1].TotalSeconds.Should().BeApproximately(s.Min.TotalSeconds, s.Min.TotalSeconds * s.Spread * 2);
         s[100].TotalSeconds.Should().BeApproximately(s.Max.TotalSeconds, s.Max.TotalSeconds * s.Spread * 2);
+    }
+
+    [Theory]
+    [InlineData(10, 100, 0.1, 2)]
+    [InlineData(2, 10, 0, 1.1)]
+    [InlineData(0, 5, 0.2, 1.5)]
+    public void ExpShouldStartFromMin(double min, double max, double spread, double multiplier)
+    {
+        // arrange
+        var seq = RetryDelaySeq.Exp(min, max, spread, multiplier);
+
+        // act, assert
+        seq[0].Should().BeGreaterOrEqualTo(TimeSpan.FromSeconds(min - spread));
     }
 }
