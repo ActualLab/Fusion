bdf09740b11d85cc14f192b1e24d818f1e7f2048
Alex Yakunin <alex.yakunin@gmail.com>
2024-07-14 22:43:44 -0700

    chore: minor edit

--- src/ActualLab.Rpc/RpcServiceMode.cs
+++ src/ActualLab.Rpc/RpcServiceMode.cs
@@ -3,7 +3,7 @@ namespace ActualLab.Rpc;
 public enum RpcServiceMode
 {
     Default = 0,
-    Local, // Singleton w/o client or RPC exposure
+    Local, // Singleton, no client or RPC exposure
     Client, // Client only, the implementation is ignored
     Server, // IService -> Service; IService is exposed via RPC
     ServerAndClient, // IService is a client invoking a local Server when possible; IService is exposed via RPC
