419bdc2c27f35b23919841189aeac75d6f5aecef
Alex Yakunin <alex.yakunin@gmail.com>
2024-07-14 22:40:20 -0700

    chore: add comments

--- src/ActualLab.Rpc/RpcServiceMode.cs
+++ src/ActualLab.Rpc/RpcServiceMode.cs
@@ -3,11 +3,11 @@ namespace ActualLab.Rpc;
 public enum RpcServiceMode
 {
     Default = 0,
-    Local,
-    Client,
-    Server,
-    ServerAndClient,
-    Hybrid,
+    Local, // Singleton w/o client or RPC exposure
+    Client, // Client only, the implementation is ignored
+    Server, // IService -> Service; IService is exposed via RPC
+    ServerAndClient, // IService is a client invoking a local Server when possible; IService is exposed via RPC
+    Hybrid, // Service is a client+server, IService is exposed via RPC.
 }
 
 public static class RpcServiceModeExt
