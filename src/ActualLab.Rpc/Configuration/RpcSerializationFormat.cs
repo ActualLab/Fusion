f572f98bec1eb134b7b2840f07b9b65e9be8e740
Alex Yakunin <alex.yakunin@gmail.com>
2024-10-11 17:55:44 -0700

    fix: RpcSerializationFormat.cs

--- src/ActualLab.Rpc/Configuration/RpcSerializationFormat.cs
+++ src/ActualLab.Rpc/Configuration/RpcSerializationFormat.cs
@@ -10,41 +10,41 @@ public sealed class RpcSerializationFormat(
 {
     // Static members
 
-   private static readonly RpcSerializationFormat SystemJson = new("json",
+   public static readonly RpcSerializationFormat SystemJson = new("json",
             () => new RpcTextArgumentSerializer(SystemJsonSerializer.Default),
             peer => new RpcTextMessageSerializer(peer));
-   private static readonly RpcSerializationFormat NewtonsoftJson = new("njson",
+   public static readonly RpcSerializationFormat NewtonsoftJson = new("njson",
             () => new RpcTextArgumentSerializer(NewtonsoftJsonSerializer.Default),
             peer => new RpcTextMessageSerializer(peer));
 
-   private static readonly RpcSerializationFormat MemoryPackV1 = new("mempack1",
+   public static readonly RpcSerializationFormat MemoryPackV1 = new("mempack1",
             () => new RpcByteArgumentSerializerV1(MemoryPackByteSerializer.Default),
             _ => MemoryPackByteSerializer.Default.ToTyped<RpcMessageV1>().Convert(RpcMessageV1.Converter));
-   private static readonly RpcSerializationFormat MemoryPackV2 = new("mempack2",
+   public static readonly RpcSerializationFormat MemoryPackV2 = new("mempack2",
             () => new RpcByteArgumentSerializer(MemoryPackByteSerializer.Default),
             peer => new RpcByteMessageSerializer(peer));
-   private static readonly RpcSerializationFormat MemoryPackV2C = new("mempack2c",
+   public static readonly RpcSerializationFormat MemoryPackV2C = new("mempack2c",
             () => new RpcByteArgumentSerializer(MemoryPackByteSerializer.Default),
             peer => new RpcByteMessageSerializerCompact(peer));
-   private static readonly RpcSerializationFormat MemoryPackV2A = new("mempack2a",
+   public static readonly RpcSerializationFormat MemoryPackV2A = new("mempack2a",
             () => new RpcByteArgumentSerializer(MemoryPackByteSerializer.Default),
             _ => MemoryPackByteSerializer.Default.ToTyped<RpcMessage>());
 
-   private static readonly RpcSerializationFormat MessagePackV1 = new("msgpack1",
+   public static readonly RpcSerializationFormat MessagePackV1 = new("msgpack1",
             () => new RpcByteArgumentSerializerV1(MessagePackByteSerializer.Default),
             _ => MessagePackByteSerializer.Default.ToTyped<RpcMessageV1>().Convert(RpcMessageV1.Converter));
-   private static readonly RpcSerializationFormat MessagePackV2 = new("msgpack2",
+   public static readonly RpcSerializationFormat MessagePackV2 = new("msgpack2",
             () => new RpcByteArgumentSerializer(MessagePackByteSerializer.Default),
             peer => new RpcByteMessageSerializer(peer));
-   private static readonly RpcSerializationFormat MessagePackV2C = new("msgpack2c",
+   public static readonly RpcSerializationFormat MessagePackV2C = new("msgpack2c",
             () => new RpcByteArgumentSerializer(MessagePackByteSerializer.Default),
             peer => new RpcByteMessageSerializerCompact(peer));
-   private static readonly RpcSerializationFormat MessagePackV2A = new("msgpack2a",
+   public static readonly RpcSerializationFormat MessagePackV2A = new("msgpack2a",
             () => new RpcByteArgumentSerializer(MessagePackByteSerializer.Default),
             _ => MessagePackByteSerializer.Default.ToTyped<RpcMessage>());
 
    // ReSharper disable once UseCollectionExpression
-   private static readonly ImmutableArray<RpcSerializationFormat> All = ImmutableArray.Create(
+   public static ImmutableArray<RpcSerializationFormat> All { get; set; } = ImmutableArray.Create(
         MemoryPackV1, MemoryPackV2, MemoryPackV2C, MemoryPackV2A,
         MessagePackV1, MessagePackV2, MessagePackV2C, MessagePackV2A,
         SystemJson, NewtonsoftJson);
