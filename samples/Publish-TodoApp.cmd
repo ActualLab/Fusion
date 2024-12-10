0e111d8d732618fba2ba76b6748625e43002226e
Alex Yakunin <alex.yakunin@gmail.com>
2024-12-10 02:31:42 -0800

    fix: remaining publish issues for TodoApp

--- samples/Publish-TodoApp.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set ASPNETCORE_ENVIRONMENT=Development
-dotnet publish TodoApp/Host/Host.csproj -p:CanPublish=true -c:Debug
---
+++ samples/Run-Published-TodoApp.cmd
@@ -0,0 +1,5 @@
+@echo off
+dotnet publish TodoApp/Host/Host.csproj -p:CanPublish=true
+
+set ASPNETCORE_ENVIRONMENT=Development
+start /D ..\artifacts\samples\publish\Host\release Samples.TodoApp.Host.exe 
