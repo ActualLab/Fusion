b66ec18d2886e59837c26788689a176fa4f8051b
Alex Yakunin <alex.yakunin@gmail.com>
2025-08-05 21:55:49 -0700

    refactor: simplify RpcHostPeerRef constructor and initialize HostId directly

--- samples/MeshRpc/Rpc/RpcHostPeerRef.cs
+++ samples/MeshRpc/Rpc/RpcHostPeerRef.cs
@@ -2,15 +2,13 @@ using ActualLab.Rpc;
 
 namespace Samples.MeshRpc;
 
-public sealed class RpcHostPeerRef : RpcPeerRef, IMeshPeerRef
+public sealed class RpcHostPeerRef(HostRef hostRef)
+    : RpcPeerRef(hostRef.ToString()), IMeshPeerRef
 {
     private static readonly ConcurrentDictionary<HostRef, RpcHostPeerRef> Cache = new();
 
-    public string HostId { get; }
+    public string HostId { get; } = hostRef.Id;
 
     public static RpcHostPeerRef Get(HostRef hostRef)
         => Cache.GetOrAdd(hostRef, key => new RpcHostPeerRef(key));
-
-    public RpcHostPeerRef(HostRef hostRef) : base(hostRef.ToString())
-        => HostId = hostRef.Id;
 }
