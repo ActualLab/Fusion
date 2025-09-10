dfb5e1eb43617e6f1ac1b93ef080c63447271f34
Alex Yakunin <alex.yakunin@gmail.com>
2025-09-09 21:59:34 -0700

    fix: TransformTest.cs

--- tests/ActualLab.Tests/Channels/TransformTest.cs
+++ tests/ActualLab.Tests/Channels/TransformTest.cs
@@ -39,7 +39,7 @@ public class TransformTest
             await transform(cSource, cTarget).ConfigureAwait(false);
 
             var elapsed = start.ElapsedMilliseconds;
-            var target = cTarget.Reader.ToAsyncEnumerable().ToListAsync();
+            var target = await cTarget.Reader.ToAsyncEnumerable().ToListAsync();
             target.Should().BeEquivalentTo(source);
             var expectedRounds = itemCount / concurrencyLevel +
                 (itemCount % concurrencyLevel != 0 ? 1 : 0);
