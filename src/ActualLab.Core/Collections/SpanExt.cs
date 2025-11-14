fde7d75d0548ef9a050c6c004703e1730778efb7
Alex Yakunin <alex.yakunin@gmail.com>
2025-11-13 16:27:03 -0800

    refactor: convert SpanExt methods to extensions for improved readability and maintainability

--- src/ActualLab.Core/Collections/SpanExt.cs
+++ src/ActualLab.Core/Collections/SpanExt.cs
@@ -16,61 +16,73 @@ public static partial class SpanExt
 
     // ReadUnchecked
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T ReadUnchecked<T>(this Span<byte> span)
+    extension(Span<byte> span)
     {
-        ref var byteRef = ref MemoryMarshal.GetReference(span);
-        return Unsafe.ReadUnaligned<T>(ref byteRef);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T ReadUnchecked<T>()
+        {
+            ref var byteRef = ref MemoryMarshal.GetReference(span);
+            return Unsafe.ReadUnaligned<T>(ref byteRef);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T ReadUnchecked<T>(int byteOffset)
+        {
+            ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
+            return Unsafe.ReadUnaligned<T>(ref byteRef);
+        }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T ReadUnchecked<T>(this Span<byte> span, int byteOffset)
+    extension(ReadOnlySpan<byte> span)
     {
-        ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
-        return Unsafe.ReadUnaligned<T>(ref byteRef);
-    }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T ReadUnchecked<T>()
+        {
+            ref var byteRef = ref MemoryMarshal.GetReference(span);
+            return Unsafe.ReadUnaligned<T>(ref byteRef);
+        }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T ReadUnchecked<T>(this ReadOnlySpan<byte> span)
-    {
-        ref var byteRef = ref MemoryMarshal.GetReference(span);
-        return Unsafe.ReadUnaligned<T>(ref byteRef);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T ReadUnchecked<T>(this ReadOnlySpan<byte> span, int byteOffset)
-    {
-        ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
-        return Unsafe.ReadUnaligned<T>(ref byteRef);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T ReadUnchecked<T>(int byteOffset)
+        {
+            ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
+            return Unsafe.ReadUnaligned<T>(ref byteRef);
+        }
     }
 
     // WriteUnchecked
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void WriteUnchecked<T>(this Span<byte> span, T value)
+    extension(Span<byte> span)
     {
-        ref var byteRef = ref MemoryMarshal.GetReference(span);
-        Unsafe.WriteUnaligned(ref byteRef, value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void WriteUnchecked<T>(T value)
+        {
+            ref var byteRef = ref MemoryMarshal.GetReference(span);
+            Unsafe.WriteUnaligned(ref byteRef, value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void WriteUnchecked<T>(T value, int byteOffset)
+        {
+            ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
+            Unsafe.WriteUnaligned(ref byteRef, value);
+        }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void WriteUnchecked<T>(this Span<byte> span, T value, int byteOffset)
+    extension(ReadOnlySpan<byte> span)
     {
-        ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
-        Unsafe.WriteUnaligned(ref byteRef, value);
-    }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void WriteUnchecked<T>(T value)
+        {
+            ref var byteRef = ref MemoryMarshal.GetReference(span);
+            Unsafe.WriteUnaligned(ref byteRef, value);
+        }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void WriteUnchecked<T>(this ReadOnlySpan<byte> span, T value)
-    {
-        ref var byteRef = ref MemoryMarshal.GetReference(span);
-        Unsafe.WriteUnaligned(ref byteRef, value);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void WriteUnchecked<T>(this ReadOnlySpan<byte> span, T value, int byteOffset)
-    {
-        ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
-        Unsafe.WriteUnaligned(ref byteRef, value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void WriteUnchecked<T>(T value, int byteOffset)
+        {
+            ref var byteRef = ref Unsafe.Add(ref MemoryMarshal.GetReference(span), byteOffset);
+            Unsafe.WriteUnaligned(ref byteRef, value);
+        }
     }
 }
