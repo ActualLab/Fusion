e98025bd00d7a1c912df958602ca15f1a82f059f
Alex Yakunin <alex.yakunin@gmail.com>
2025-11-19 00:44:55 -0800

    test: adjust nullability in `Polymorph` method definition and implementation

--- tests/ActualLab.Tests/Rpc/Services/TestRpcBackend.cs
+++ tests/ActualLab.Tests/Rpc/Services/TestRpcBackend.cs
@@ -4,11 +4,11 @@ namespace ActualLab.Tests.Rpc;
 
 public interface ITestRpcBackend : ICommandService, IBackendService
 {
-    public Task<ITuple> Polymorph(ITuple argument, CancellationToken cancellationToken = default);
+    public Task<ITuple?> Polymorph(ITuple? argument, CancellationToken cancellationToken = default);
 }
 
 public class TestRpcBackend : ITestRpcBackend
 {
-    public virtual Task<ITuple> Polymorph(ITuple argument, CancellationToken cancellationToken = default)
+    public virtual Task<ITuple?> Polymorph(ITuple? argument, CancellationToken cancellationToken = default)
         => Task.FromResult(argument);
 }
