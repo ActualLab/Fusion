b3584f7765147fe4e22e570af702586e4a8ed29e
Sergey Yakunin <sergey@yakunins.com>
2025-11-21 17:54:04 -0800

    Part07.md

---
+++ docs/Part08.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+
+namespace Docs
+{
+    public static class Part08
+    {
+        public static async Task Run()
+        {
+            WriteLine("Part 8: Scaling Fusion Services");
+            WriteLine("This part covers scaling and doesn't have executable console examples.");
+            await Task.CompletedTask;
+        }
+    }
+}
