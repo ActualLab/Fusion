9f9fd0a65e1f0a4fb9a42c0f675d9ee307d99c43
Alex Yakunin <alex.yakunin@gmail.com>
2025-11-23 03:23:38 -0800

    chore: remove old scripts folder

--- scripts/RunCoverage-SingleCore.cmd
+++
@@ -1,6 +0,0 @@
-pushd ..
-dotnet run --project build --configuration Release --no-launch-profile -- --configuration Debug restore restore-tools
-dotnet run --project build --configuration Release --no-launch-profile -- --configuration Debug build
-set GITHUB_ACTIONS=true
-procgov64 --maxmem=4G --cpu=0x1 --recursive "dotnet.exe" "run --project build --configuration Release --no-launch-profile -- --configuration Debug coverage"
-popd
--- scripts/RunTests-SingleCore.cmd
+++
@@ -1,3 +0,0 @@
-pushd ..
-procgov64 --maxmem=4G --cpu=0x1 --recursive dotnet.exe test
-popd
