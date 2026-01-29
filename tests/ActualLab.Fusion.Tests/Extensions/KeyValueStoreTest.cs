ec74d6ec689f2dafe9ffb145f8526d0b61768e52
Alex Yakunin <alex.yakunin@gmail.com>
2026-01-29 09:58:49 -0800

    chore: update KeyValueStoreTest to use Redis operation log change tracking with in-memory key value store

--- tests/ActualLab.Fusion.Tests/Extensions/KeyValueStoreTest.cs
+++ tests/ActualLab.Fusion.Tests/Extensions/KeyValueStoreTest.cs
@@ -14,6 +14,7 @@ public abstract class KeyValueStoreTestBase : FusionTestBase
     {
         UseTestClock = true;
         UseInMemoryKeyValueStore = useInMemoryKeyValueStore;
+        UseRedisOperationLogChangeTracking = useInMemoryKeyValueStore;
     }
 
     [Fact]
