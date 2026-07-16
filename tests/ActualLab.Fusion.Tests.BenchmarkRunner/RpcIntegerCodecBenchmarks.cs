a1108767a99f0a8fafec46d7ee7d3b4d98c15311
Alex Yakunin <alex.yakunin@gmail.com>
2026-07-16 11:08:49 -0700

    Add realistic RPC argument codec benchmarks

--- tests/ActualLab.Fusion.Tests.BenchmarkRunner/RpcIntegerCodecBenchmarks.cs
+++ tests/ActualLab.Fusion.Tests.BenchmarkRunner/RpcIntegerCodecBenchmarks.cs
@@ -6,17 +6,27 @@ namespace ActualLab.Fusion.Tests.BenchmarkRunner;
 
 public class RpcIntegerCodecBenchmarks : RpcBenchmarkBase
 {
-    private ArgumentList _arguments = null!;
+    private ArgumentList _longArguments = null!;
+    private ArgumentList _stringArguments = null!;
+    private ArgumentList _threeArguments = null!;
     private ArrayPoolBuffer<byte> _buffer = null!;
-    private ReadOnlyMemory<byte> _data;
+    private ReadOnlyMemory<byte> _longData;
+    private ReadOnlyMemory<byte> _stringData;
+    private ReadOnlyMemory<byte> _threeData;
 
     protected override void OnSetup()
     {
-        _arguments = ArgumentList.New<long, CancellationToken>(16_384, default);
-        _buffer = new ArrayPoolBuffer<byte>(16);
-        Peer.ArgumentSerializer.Serialize(_arguments, false, _buffer);
-        _data = _buffer.WrittenMemory.ToArray();
-        _buffer.Reset();
+        _longArguments = ArgumentList.New<long, CancellationToken>(16_384, default);
+        _stringArguments = ArgumentList.New<string, CancellationToken>("benchmark-key", default);
+        _threeArguments = ArgumentList.New<string, long, string, CancellationToken>(
+            "benchmark-prefix",
+            16_384,
+            "benchmark-suffix",
+            default);
+        _buffer = new ArrayPoolBuffer<byte>(128);
+        _longData = SerializeOnce(_longArguments);
+        _stringData = SerializeOnce(_stringArguments);
+        _threeData = SerializeOnce(_threeArguments);
     }
 
     protected override void OnCleanup()
@@ -28,7 +38,7 @@ public class RpcIntegerCodecBenchmarks : RpcBenchmarkBase
         var totalLength = 0;
         for (var i = 0; i < BenchmarkSettings.OperationCount; i++) {
             _buffer.Reset();
-            Peer.ArgumentSerializer.Serialize(_arguments, false, _buffer);
+            Peer.ArgumentSerializer.Serialize(_longArguments, false, _buffer);
             totalLength += _buffer.WrittenCount;
         }
         return totalLength;
@@ -37,9 +47,58 @@ public class RpcIntegerCodecBenchmarks : RpcBenchmarkBase
     [Benchmark(OperationsPerInvoke = BenchmarkSettings.OperationCount)]
     public long DeserializeLong()
     {
-        var arguments = _arguments;
+        var arguments = _longArguments;
         for (var i = 0; i < BenchmarkSettings.OperationCount; i++)
-            Peer.ArgumentSerializer.Deserialize(ref arguments, false, _data);
+            Peer.ArgumentSerializer.Deserialize(ref arguments, false, _longData);
         return arguments.Get<long>(0);
+    }
+
+    [Benchmark(OperationsPerInvoke = BenchmarkSettings.OperationCount)]
+    public int SerializeString()
+    {
+        var totalLength = 0;
+        for (var i = 0; i < BenchmarkSettings.OperationCount; i++) {
+            _buffer.Reset();
+            Peer.ArgumentSerializer.Serialize(_stringArguments, false, _buffer);
+            totalLength += _buffer.WrittenCount;
+        }
+        return totalLength;
+    }
+
+    [Benchmark(OperationsPerInvoke = BenchmarkSettings.OperationCount)]
+    public string DeserializeString()
+    {
+        var arguments = _stringArguments;
+        for (var i = 0; i < BenchmarkSettings.OperationCount; i++)
+            Peer.ArgumentSerializer.Deserialize(ref arguments, false, _stringData);
+        return arguments.Get<string>(0);
+    }
+
+    [Benchmark(OperationsPerInvoke = BenchmarkSettings.OperationCount)]
+    public int SerializeStringLongString()
+    {
+        var totalLength = 0;
+        for (var i = 0; i < BenchmarkSettings.OperationCount; i++) {
+            _buffer.Reset();
+            Peer.ArgumentSerializer.Serialize(_threeArguments, false, _buffer);
+            totalLength += _buffer.WrittenCount;
+        }
+        return totalLength;
+    }
+
+    [Benchmark(OperationsPerInvoke = BenchmarkSettings.OperationCount)]
+    public long DeserializeStringLongString()
+    {
+        var arguments = _threeArguments;
+        for (var i = 0; i < BenchmarkSettings.OperationCount; i++)
+            Peer.ArgumentSerializer.Deserialize(ref arguments, false, _threeData);
+        return arguments.Get<long>(1);
+    }
+
+    private ReadOnlyMemory<byte> SerializeOnce(ArgumentList arguments)
+    {
+        _buffer.Reset();
+        Peer.ArgumentSerializer.Serialize(arguments, false, _buffer);
+        return _buffer.WrittenMemory.ToArray();
     }
 }
